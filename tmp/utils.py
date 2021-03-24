a385504a2cfb30ed017f9c72242255f1425dd9d5
Asmaasa3d <48691633+Asmaasa3d@users.noreply.github.com>
2021-03-24 23:23:52 +0200

    Update utils.py

--- tmp/utils.py
+++ tmp/utils.py
@@ -2,30 +2,33 @@ import gzip
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 import numpy as np
+import pandas as pd
 from keras.utils.data_utils import get_file
+from sklearn.model_selection import train_test_split
 
 
-def load_data(data_file, url):
+def load_data(data_file):
     """loads the data from the gzip pickled files, and converts to numpy arrays"""
     print('loading data ...')
-    path = get_file(data_file, origin=url)
-    f = gzip.open(path, 'rb')
-    train_set, valid_set, test_set = load_pickle(f)
-    f.close()
+    data = pd.read_csv(data_file,nrows=7271)
+    """split into features and labels"""
+    y = data.point
+    X = data.drop('point', axis=1)
+    """split into train and test"""
+    X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2)
+    test_set_x, valid_set_x, test_set_y, valid_set_y = train_test_split(X_test,y_test,test_size=0.5)
 
-    train_set_x, train_set_y = make_numpy_array(train_set)
-    valid_set_x, valid_set_y = make_numpy_array(valid_set)
-    test_set_x, test_set_y = make_numpy_array(test_set)
+    train_set_x = np.asarray(X_train, dtype='float32')
+    train_set_y= np.asarray(y_train, dtype='int32') 
+    test_set_x = np.asarray(test_set_x, dtype='float32')
+    test_set_y = np.asarray(test_set_y, dtype='int32') 
+    valid_set_x=np.asarray(valid_set_x, dtype='float32')
+    valid_set_y = np.asarray(valid_set_y, dtype='int32')
 
     return [(train_set_x, train_set_y), (valid_set_x, valid_set_y), (test_set_x, test_set_y)]
 
 
-def make_numpy_array(data_xy):
-    """converts the input to numpy arrays"""
-    data_x, data_y = data_xy
-    data_x = np.asarray(data_x, dtype='float32')
-    data_y = np.asarray(data_y, dtype='int32')
-    return data_x, data_y
+
 
 
 def svm_classify(data, C):
@@ -44,6 +47,28 @@ def svm_classify(data, C):
     p = clf.predict(test_data)
     test_acc = accuracy_score(test_label, p)
     p = clf.predict(valid_data)
+    valid_acc = accuracy_score(valid_label, p)
+
+    return [test_acc, valid_acc]
+
+
+def load_pickle(f):
+    """
+    loads and returns the content of a pickled file
+    it handles the inconsistencies between the pickle packages available in Python 2 and 3
+    """
+    try:
+        import cPickle as thepickle
+    except ImportError:
+        import _pickle as thepickle
+
+    try:
+        ret = thepickle.load(f, encoding='latin1')
+    except TypeError:
+        ret = thepickle.load(f)
+
+    return ret
+
     valid_acc = accuracy_score(valid_label, p)
 
     return [test_acc, valid_acc]
